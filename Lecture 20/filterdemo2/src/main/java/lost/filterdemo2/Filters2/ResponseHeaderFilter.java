087cffeeb8e4fea7883c1ce19a06d15531f09165
kartik <kartikchauhan0895@gmail.com>
2026-08-20 17:54:27 +0530

    updated lecture 20

--- Lecture 20/filterdemo2/src/main/java/lost/filterdemo2/Filters2/ResponseHeaderFilter.java
+++ Lecture 20/filterdemo2/src/main/java/lost/filterdemo2/Filters2/ResponseHeaderFilter.java
@@ -3,10 +3,12 @@ package lost.filterdemo2.Filters2;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@Component
 public class ResponseHeaderFilter implements Filter {
 
     @Override
@@ -18,12 +20,15 @@ public class ResponseHeaderFilter implements Filter {
         HttpServletRequest httpRequest=
                 (HttpServletRequest) request;
 
-        HttpServletResponse httpResponse=(HttpServletResponse) response;
+        HttpServletResponse httpResponse=
+                (HttpServletResponse) response;
 
         String requestId = UUID.randomUUID().toString();
 
 
         httpResponse.setHeader("X-Request-ID",requestId);
+
+        chain.doFilter(request,response);
 
 
     }
