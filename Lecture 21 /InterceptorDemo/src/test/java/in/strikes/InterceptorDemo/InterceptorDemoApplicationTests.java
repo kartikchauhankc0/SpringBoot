2b8b5fb330359bbe5dda035d486eabdc3e123412
kartik <kartik.chauhan@legistify.com>
2026-09-10 12:29:18 +0530

    updated lecture 21

---
+++ Lecture 21 /InterceptorDemo/src/test/java/in/strikes/InterceptorDemo/InterceptorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.strikes.InterceptorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
