f06e3dd1365f12f605d689e82174bf682a1beb01
kartik <kartik.chauhan@legistify.com>
2026-09-21 17:13:39 +0530

    starting lecture 27

---
+++ Lecture 27/springJdbc/src/main/java/in/raft/springJdbc/controller/StudentController.java
@@ -0,0 +1,51 @@
+package in.raft.springJdbc.controller;
+
+
+import in.raft.springJdbc.model.Student;
+import in.raft.springJdbc.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createStudent(@RequestBody Student student) {
+        studentService.createStudent(student);
+        return ResponseEntity.ok("DONE");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+        Student student = studentService.getStudentById(id);
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateStudent(@RequestBody Student student) {
+        studentService.updateStudent(student);
+        return ResponseEntity.ok("DONE");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok("DONE");
+    }
+}
---
+++ Lecture 27/springJdbc/src/main/java/in/raft/springJdbc/service/StudentService.java
@@ -0,0 +1,38 @@
+package in.raft.springJdbc.service;
+
+
+import in.raft.springJdbc.model.Student;
+import in.raft.springJdbc.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void createStudent(Student student) {
+        studentRepository.createStudent(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.getStudent();
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.getStudentById(id);
+    }
+
+    public void updateStudent(Student student) {
+        studentRepository.updateStudent(student, student.getId());
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteStudent(id);
+    }
+}
